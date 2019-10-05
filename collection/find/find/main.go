a69fc058a774604c371f4b88671ae1a164bf009d
yangchengkai1 <1351599172@qq.com>
2019-10-06 00:02:56 +0800

    add find example

---
+++ collection/find/find/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func main() {
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://root:single@localhost:27017"))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect(context.Background())
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	collection := client.Database("simple").Collection("article")
+
+	cursor, err := collection.Find(context.Background(), bson.M{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("successful", cursor)
+}
