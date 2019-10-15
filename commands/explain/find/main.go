099d09bb8a3f5b38fdb3b3b36b75865c290d67cd
yangchengkai1 <1351599172@qq.com>
2019-10-15 08:49:40 +0800

    Create main.go

---
+++ commands/explain/find/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func main() {
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://root:single@localhost:27017"))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect(context.Background())
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	collection := client.Database("index").Collection("test")
+
+	result := collection.Database().RunCommand(context.Background(), bson.M{
+		"explain": bson.M{
+			"find": bson.M{"status": 1},
+		},
+	})
+
+	if result.Err() != nil {
+		raw, _ := result.DecodeBytes()
+		log.Println(raw.String())
+		return
+	}
+
+	raw, _ := result.DecodeBytes()
+
+	log.Println(raw.String())
+
+}
